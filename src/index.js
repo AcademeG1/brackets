470697d00e93474f97728dd1959e17a0cee9893b
academeg1 <m-ignatovich@hotmail.com>
2023-01-23 12:11:12 +0300

    feat: pass 20

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const strArr = str.split('');
+    const spliceBrecets = () => {
+        strArr.forEach((bracket, index) => {
+            bracketsConfig.forEach(item => {
+                if (item[0] == bracket && item[1] == strArr[index+1]) {
+                    strArr.splice(index, 2);
+                    if (strArr.length > 0) {
+                        spliceBrecets();
+                    }
+                }
+            })
+        })
+    }
+    spliceBrecets();
+    if (strArr.length > 0) {
+        return false
+    } else {
+        return true
+    }
 }
